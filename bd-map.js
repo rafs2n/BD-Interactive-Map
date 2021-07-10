586b02b5b0e4702eccd00922ae05801d50c1e760
Abdul Aziz <md.aziz@northsouth.edu>
2021-07-10 16:50:51 +0600

    image slider added

--- bd-map.js
+++ bd-map.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
     $('#mySVG').load("bd-map.html", function () {
 
         $("#bd-map").click(function (evt) {         //when user click evt will return the id
-            
+
             $("#bd-map").find('path').removeClass('active');
             let targ = evt.target.tagName === 'path' ? $(evt.target) : $(evt.target).parent().prev('path');
             console.log(targ.attr('id'))
             targ.addClass('active')
-            switch (targ.attr('id')){
-
-            // switch (evt.target.id) {
+            switch (targ.attr('id')) {
                 case "panchagarh":
+
+                    $('#card-header').html('<h4>Pachagarh | পঞ্চগড়</h4>')
                     $('#info').load('info.html #panchagarh')     //loading data from info.html
                     break;
                 case "bandarban":
@@ -25,6 +25,9 @@ $(document).ready(function () {
                     $('#card-header').html('<h4>Sylhet | সিলেট</h4>')
                     $('#info').load('info.html #sylhet')
                     break;
+                case "dhaka":
+                    $('#card-header').html('<h4>Dhaka | ঢাকা</h4>')
+                    $('#info').load('info.html #dhaka')
             }
         });
     });
