8404d1829cf829fc55cb7c96390793ea97f5e1b4
Abdul Aziz <md.aziz@northsouth.edu>
2021-08-06 15:22:11 +0600

    function added

--- bd-map.js
+++ bd-map.js
@@ -1,3 +1,14 @@
+/*
+*
+* Title: Bangladesh | A Digital Interactive Map
+* Description: This JS file contain all the js function to control the application.
+* Author: Abdul Aziz (Github: rafs2n)
+* Date: 05/07/2021
+*
+*/
+
+
+
 $(document).ready(function () {
     $('#mySVG').load("bd-map.html", function () {
 
@@ -9,28 +20,57 @@ $(document).ready(function () {
             targ.addClass('active')
             switch (targ.attr('id')) {
                 case "panchagarh":
-
                     $('#card-header').html('<h4>Pachagarh | পঞ্চগড়</h4>')
-                    $('#info').load('info.html #panchagarh')     //loading data from info.html
+                    $('#info').load('db.html #panchagarh')     //loading data from db.html
+                    $("#population").load("db.html #panchagarh-population")
+                    $("#volume").load("db.html #panchagarh-volume")
                     break;
+
                 case "bandarban":
                     $('#card-header').html('<h4>Bandarban | বান্দরবান</h4>')
-                    $('#info').load('info.html #bandarban')
+                    $('#info').load('db.html #bandarban')
+                    $("#population").load("db.html #bandarban-population")
+                    $("#volume").load("db.html #bandarban-volume")
                     break;
+
+                case "rangamati":
+                    $('#card-header').html('<h4>Rangamati | রাঙ্গামাটি</h4>')
+                    $('#info').load('db.html #rangamati')
+                    $("#population").load("db.html #rangamati-population")
+                    $("#volume").load("db.html #rangamati-volume")
+                    break;
+
+                case "khagrachari":
+                    $('#card-header').html('<h4>Khagrachari | খাগড়াছড়ি</h4>')
+                    $('#info').load('db.html #khagrachari')
+                    $("#population").load("db.html #khagrachari-population")
+                    $("#volume").load("db.html #khagrachari-volume")
+                    break;
+
                 case "comilla":
                     $('#card-header').html('<h4>Comilla | কুমিল্লা</h4>')
-                    $('#info').load('info.html #comilla')
-                    $("#ayoton").val("৩০৮৭.৩৩ বর্গ কিলোমিটার")
-                    $("#population").val("৫৩,৮৭,২৮৮ জন")
+                    $('#info').load('db.html #comilla')
+                    $("#population").load("db.html #comilla-population")
+                    $("#volume").load("db.html #comilla-volume")
+                    // $("#food").load("db.html #comilla-food")
+                    // $("#tourist-place").load("db.html #comilla-tourist-place")
                     break;
 
                 case "sylhet":
                     $('#card-header').html('<h4>Sylhet | সিলেট</h4>')
-                    $('#info').load('info.html #sylhet')
+                    $('#info').load('db.html #sylhet')
+                    $("#population").load("db.html #sylhet-population")
+                    $("#volume").load("db.html #sylhet-volume")
+                    // $("#food").load("db.html #sylhet-food")
+                    // $("#tourist-place").load("db.html #sylhet-tourist-place")
                     break;
                 case "dhaka":
                     $('#card-header').html('<h4>Dhaka | ঢাকা</h4>')
-                    $('#info').load('info.html #dhaka')
+                    $('#info').load('db.html #dhaka')
+                    $("#population").load("db.html #dhaka-population")
+                    $("#volume").load("db.html #dhaka-volume")
+                    break;
+
             }
         });
     });
